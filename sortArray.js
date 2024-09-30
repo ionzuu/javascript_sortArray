0d3a41da421589a8d552fc7aad302daa5786a4a8
Jonathan Costilla <99742932+ionzuu@users.noreply.github.com>
2024-09-29 22:32:45 -0500

    Add files via upload

---
+++ sortArray.js
@@ -0,0 +1,7 @@
+// Task 7: Sort an Array
+// Write a function that sorts an array of numbers in ascending order (without using built-in methods like sort).
+
+function sortArray(arr){
+    return (console.log("Array: ", arr, "Array sorted: ", arr.sort()))
+}
+console.log(sortArray([5, 4, 3, 6, 2, 1, 7, 8]))
